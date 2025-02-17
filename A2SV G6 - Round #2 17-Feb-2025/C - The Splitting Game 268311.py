0bdb4342a6f50e8c3caa1fba46512b3a1edb5dfb
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-17 10:57:04 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/C - The Splitting Game 268311.py
@@ -0,0 +1,18 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter,defaultdict
+t = int(input())
+while t:
+    t-=1
+    n=int(input())
+    s=input()
+    first=Counter(s)
+    second=defaultdict(int)
+    max_sum=len(first)
+    for i in s:
+        first[i]-=1
+        if first[i]==0:
+            del first[i]
+        second[i]+=1
+        max_sum=max(max_sum,len(first)+len(second))
+    print(max_sum)
