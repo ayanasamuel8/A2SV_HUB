7767fddfd1bc7b45084f33975f897414e63116bf
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-17 10:57:09 +0300

    Created 'B - Special Matrix Quest' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/B - Special Matrix Quest 268313.py
@@ -0,0 +1,14 @@
+# Problem: B - Special Matrix Quest - https://codeforces.com/gym/586960/problem/B
+
+n = int(input())
+matrix=[]
+for i in range(n):
+    matrix.append(list(map(int,input().split())))
+max_sum=0
+half=n//2
+for i in range(n):
+    max_sum+=matrix[i][i]
+    max_sum+=matrix[i][n-i-1]
+    max_sum+=matrix[i][half]
+    max_sum+=matrix[half][i]
+print(max_sum-matrix[half][half]*3)
