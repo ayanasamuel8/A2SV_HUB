f1e93df849af55795a4cfec533c95c03f39f3d6b
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-17 10:58:54 +0300

    Created 'C - Compare T-Shirt Sizes' by A2SV Hub

---
+++ A2SV G6 - Round #1 17-Feb-2025/C - Compare T-Shirt Sizes 268320.py
@@ -0,0 +1,23 @@
+# Problem: C - Compare T-Shirt Sizes - https://codeforces.com/gym/585107/problem/C
+
+t=int(input())
+while t:
+    t-=1
+    s1,s2=input().split()
+    if (s1[-1]!=s2[-1]):
+        if (s1[-1]=='S'):
+            print('<')
+        elif (s1[-1]=='L'):
+            print('>')
+        elif (s2[-1]=='S'):
+            print('>')
+        elif (s2[-1]=='L'):
+            print('<')
+    else:
+        if len(s1)==len(s2):
+            print('=')
+        else:
+            if(s1[-1]=='L'):
+                print('>' if len(s1)>len(s2) else '<')
+            if(s1[-1]=='S'):
+                print('>' if len(s1)<len(s2) else '<')
