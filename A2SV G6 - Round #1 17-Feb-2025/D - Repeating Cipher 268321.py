84f1d562fd32f64423e012e8294fc8288e6e5828
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-17 10:59:02 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 17-Feb-2025/D - Repeating Cipher 268321.py
@@ -0,0 +1,16 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n=int(input())
+s=input()
+i=0
+add=1
+ans=''
+while i<n:
+    j=i+1
+    for j in range(i+1,min(i+add,n)):
+        if s[j]!=s[i]:
+            break
+    ans+=s[i]
+    add+=1
+    i=j+1
+print(ans)
