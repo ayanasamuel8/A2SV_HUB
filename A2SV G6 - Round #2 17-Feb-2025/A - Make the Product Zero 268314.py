228d64e1e8a75a10faf2a5ddeda920da8247b43f
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-17 10:57:16 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 17-Feb-2025/A - Make the Product Zero 268314.py
@@ -0,0 +1,8 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+n = int(input())
+array=list(map(int,input().split()))
+minel=float('inf')
+for i in range(n):
+    minel=min(minel,abs(array[i]))
+print(minel)
