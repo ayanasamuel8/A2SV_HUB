71aedd69e7a338e936ee6eb8ea158a605b4f2605
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-17 10:59:09 +0300

    Created 'B - Square String?' by A2SV Hub

---
+++ A2SV G6 - Round #1 17-Feb-2025/B - Square String? 268322.py
@@ -0,0 +1,11 @@
+# Problem: B - Square String? - https://codeforces.com/gym/585107/problem/B
+
+t=int(input())
+while t:
+    t-=1
+    s=input()
+    for i in range(len(s)+1):
+        if s[:i]==s[i:]:
+            print("YES")
+            break
+    else: print("NO")
