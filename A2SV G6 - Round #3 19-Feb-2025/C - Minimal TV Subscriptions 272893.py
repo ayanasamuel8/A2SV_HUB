6c2c1fbfb4c0c3df71acbf000988d429a444f40d
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-19 09:23:08 +0300

    Created 'C - Minimal TV Subscriptions' by A2SV Hub

---
+++ A2SV G6 - Round #3 19-Feb-2025/C - Minimal TV Subscriptions 272893.py
@@ -0,0 +1,19 @@
+# Problem: C - Minimal TV Subscriptions - https://codeforces.com/gym/588468/problem/C
+
+from collections import defaultdict, Counter
+t = int(input())
+while t:
+    t -= 1
+    n,k,d = map(int,input().split())
+    array = list(map(int,input().split()))
+    uniques = defaultdict(int)
+    for i in range(d):
+        uniques[array[i]] += 1
+    ans = len(uniques)
+    for i in range(d,n):
+        uniques[array[i-d]] -= 1
+        uniques[array[i]] += 1
+        if uniques[array[i-d]] == 0:
+            del uniques[array[i-d]]
+        ans = min(ans,len(uniques))
+    print(ans)
