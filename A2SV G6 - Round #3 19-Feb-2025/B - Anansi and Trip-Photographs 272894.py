06c32b6d0bfd4337cf0e01c3259feb7790df02ec
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-19 09:23:23 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 19-Feb-2025/B - Anansi and Trip-Photographs 272894.py
@@ -0,0 +1,14 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+t = int(input())
+while t:
+    t -= 1
+    n,k = map(int,input().split())
+    array = list(map(int,input().split()))
+    array.sort()
+    for i in range(n):
+        if array[i+n]-array[i]<k:
+            print('NO')
+            break
+    else:
+        print('YES')
