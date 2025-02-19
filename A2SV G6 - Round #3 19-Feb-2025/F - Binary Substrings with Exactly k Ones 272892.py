b1fdd8985effb3cb7333c25dd01bb21babc7c9a2
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-19 09:22:57 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 19-Feb-2025/F - Binary Substrings with Exactly k Ones 272892.py
@@ -0,0 +1,20 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+k = int(input())
+s = input()
+def count(l):
+    if l<0: return 0
+    ones_count = left = 0
+    ans = 0
+    for right in range(len(s)):
+        ones_count += int(s[right])
+        while ones_count > l:
+            ans += right - left
+            ones_count -= int(s[left])
+            left += 1
+    while left<len(s):
+        ans += len(s) - left
+        ones_count -= int(s[left])
+        left += 1
+    return ans 
+print(count(k)-count(k-1))
