51d74053905951bddcf142498eb34854f0e53c17
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-02-21 09:08:45 +0300

    Created 'D - Bernabas and the Harmonious Melody' by A2SV Hub

---
+++ A2SV G6 - Round #3 21-Feb-2025/D - Bernabas and the Harmonious Melody 277799.py
@@ -0,0 +1,27 @@
+# Problem: D - Bernabas and the Harmonious Melody - https://codeforces.com/gym/588468/problem/D
+
+t = int(input())
+while t:
+    t -= 1
+    n = int(input())
+    string = input()
+    elements = set(string)
+    ans = float('inf')
+    for i in elements:
+        count = 0
+        left, right = 0, len(string)-1
+        f = True
+        while left < right:
+            if string[left] != string[right]:
+                count += 1
+                if string[left] == i : left +=1
+                elif string[right] == i: right -= 1
+                else:
+                    f = False 
+                    break
+            else:
+                left += 1
+                right -= 1
+        if f :
+            ans = min(ans, count)
+    print(ans if ans != float('inf') else -1)
