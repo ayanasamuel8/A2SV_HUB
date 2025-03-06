8a19099d4c855d7de09ce039f5f4bbbc5eb9aaa8
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-03-06 21:30:23 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 06-Mar-2025/Array Splitting 295165.py
@@ -0,0 +1,30 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+import sys, math
+from collections import Counter, defaultdict,deque
+from functools import cmp_to_key
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+INT = lambda: int(input().strip())
+INTS = lambda: list(map(int, input().split()))
+LIST = lambda: list(map(int, input().split()))
+LINE = lambda: input().strip()
+MATRIX = lambda n, m: [list(map(int, input().split())) for _ in range(n)]
+
+def solve():
+    n, k = INTS()
+    array = LIST()
+    difference = [0] * n
+    for i in range(n - 1):
+        difference[i] = abs(array[i + 1] - array[i])
+    difference.sort()
+    difference = difference[:n - k + 1]
+    output(f"{sum(difference)}")
+
+def main():
+    solve()
+
+if __name__ == '__main__':
+    main()
