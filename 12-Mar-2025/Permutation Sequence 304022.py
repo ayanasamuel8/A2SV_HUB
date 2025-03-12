f413a54be2b15437f2413d28f26b17a5be088f5a
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-03-12 17:44:22 +0300

    Created 'Permutation Sequence' by A2SV Hub

---
+++ 12-Mar-2025/Permutation Sequence 304022.py
@@ -0,0 +1,22 @@
+# Problem: Permutation Sequence - https://leetcode.com/problems/permutation-sequence/description/
+
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        result = []
+        seen = [False] * (n + 1)
+        def backTrack(path):
+            if len(path) == n:
+                result.append(path[:])
+            if len(result) > k:
+                return
+            for i in range(1, n + 1):
+                if not seen[i] :
+                    path.append(i)
+                    seen[i] = True
+                    backTrack(path)
+                    seen[i] = False
+                    path.pop()
+        backTrack([])
+        ans = result[k - 1]
+        return ''.join(map(str,ans))
+            
