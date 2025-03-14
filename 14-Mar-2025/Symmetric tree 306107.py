12c05f5eb3d5a05f087ce559ade9a22f35eaec5a
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-03-14 07:59:28 +0300

    Created 'Symmetric tree' by A2SV Hub

---
+++ 14-Mar-2025/Symmetric tree 306107.py
@@ -0,0 +1,19 @@
+# Problem: Symmetric tree - https://leetcode.com/problems/symmetric-tree/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeVal(self, head, val):
+        if not head or not head.next: return
+        if head.next.val == val:
+            head.next = head.next.next
+        else:
+            head = head.next
+        self.removeVal(head, val)
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(0,head)
+        self.removeVal(dummy, val)
+        return dummy.next
