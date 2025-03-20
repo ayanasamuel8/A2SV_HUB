823462a5ba321a3c66ba8245b2b02758b4414d00
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-03-20 18:37:17 +0300

    Created 'N Queens II' by A2SV Hub

---
+++ 20-Mar-2025/N Queens II 313385.py
@@ -0,0 +1,24 @@
+# Problem: N Queens II - https://leetcode.com/problems/n-queens-ii/
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        colseen = set()
+        maindiagonal = set()
+        secondarydiagonal = set()
+        self.count = 0
+        def backTrack(row):
+            if row >= n:
+                return
+            for i in range(n):
+                if i not in colseen and row - i not in maindiagonal and row + i not in secondarydiagonal:
+                    colseen.add(i)
+                    maindiagonal.add(row - i)
+                    secondarydiagonal.add(row + i)
+                    if row == n - 1:
+                        self.count += 1
+                    backTrack(row + 1)
+                    colseen.remove(i)
+                    maindiagonal.remove(row - i)
+                    secondarydiagonal.remove(row + i)
+        backTrack(0)
+        return self.count
