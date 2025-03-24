34e131b59c7239364e11c443b5d03ea53b8d4c50
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-03-24 09:17:34 +0300

    Created 'The k-th Lexicographical String of All Happy Strings of Length n' by A2SV Hub

---
+++ 24-Mar-2025/The k-th Lexicographical String of All Happy Strings of Length n 316504.py
@@ -0,0 +1,19 @@
+# Problem: The k-th Lexicographical String of All Happy Strings of Length n - https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/
+
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        string = 'abc'
+        result = []
+
+        def backTrack(path):
+            if len(path) == n+1:
+                result.append(''.join(path[1:]))
+                return
+            for i in range(3):
+                if path[-1] != string[i]:
+                    path.append(string[i])
+                    backTrack(path)
+                    path.pop()
+            return
+        backTrack([0])
+        return result[k-1] if k-1<len(result) else ''
