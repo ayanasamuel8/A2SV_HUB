7578046cbd417cdb1be4922883813f9dd7ca371a
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-03-24 10:31:14 +0300

    Created 'N Queens' by A2SV Hub

---
+++ 24-Mar-2025/N Queens 316532.py
@@ -0,0 +1,32 @@
+# Problem: N Queens - https://leetcode.com/problems/n-queens/
+
+class Solution:
+
+    def backTrack(self, row, n):
+        if row >= n:
+            possible = []
+            for i in self.board:
+                possible.append(''.join(i))
+            self.result.append(possible)
+        
+        for i in range(n):
+            if i not in self.cols and row - i not in self.diagonal and row + i not in self.secondary:
+                self.cols.add(i)
+                self.diagonal.add(row - i)
+                self.secondary.add(row + i)
+                self.board[row][i] = 'Q'
+                self.backTrack(row + 1, n)
+                self.board[row][i] = '.'
+                self.cols.remove(i)
+                self.diagonal.remove(row - i)
+                self.secondary.remove(row + i)
+
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        self.result = []
+        self.board = [['.'] * n for _ in range(n)]
+        self.cols = set()
+        self.diagonal = set()
+        self.secondary = set()
+        self.backTrack(0, n)
+        return self.result
