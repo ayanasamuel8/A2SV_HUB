e4454a8cb40399f02da80ff576cffed72283c415
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-03-28 21:51:35 +0300

    Created 'Heaters ' by A2SV Hub

---
+++ 28-Mar-2025/Heaters  321205.py
@@ -0,0 +1,26 @@
+# Problem: Heaters  - https://leetcode.com/problems/heaters/
+
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        heaters.sort()
+        houses.sort()
+        left, right = 0, max(max(houses), max(heaters))
+        answer = right
+
+        def is_possible(radius):
+            i = 0
+            for house in houses:
+                while i < len(heaters) and heaters[i] < house - radius:
+                    i += 1
+                if i >= len(heaters) or heaters[i] > house + radius:
+                    return False
+            return True
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if is_possible(mid):
+                answer = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return answer
