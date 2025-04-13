35e48e159ea278350751618226991882c0d873f4
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-04-13 10:59:28 +0300

    Created 'Destroying Bridges' by A2SV Hub

---
+++ 13-Apr-2025/Destroying Bridges 332816.py
@@ -0,0 +1,10 @@
+# Problem: Destroying Bridges - https://codeforces.com/problemset/problem/1944/A
+
+t = int(input())
+while t:
+    t -= 1
+    n,k = map(int,input().split())
+    if k >= n - 1:
+        print(1)
+    else:
+        print(n)
