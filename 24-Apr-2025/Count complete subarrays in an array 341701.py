390c7a86bbf25d2c9f68d220d42793060696fbec
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-04-24 20:54:43 +0300

    Created 'Count complete subarrays in an array' by A2SV Hub

---
+++ 24-Apr-2025/Count complete subarrays in an array 341701.py
@@ -0,0 +1,18 @@
+# Problem: Count complete subarrays in an array - https://leetcode.com/problems/count-complete-subarrays-in-an-array/
+
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        complete = len(set(nums))
+        left = 0
+        window = defaultdict(int)
+        count = 0
+        for right in range(n):
+            window[nums[right]] += 1
+            while len(window) == complete:
+                count += (n - right)
+                window[nums[left]] -= 1
+                if window[nums[left]] == 0:
+                    del window[nums[left]]
+                left += 1
+        return count
