bb7da2a32e95adefbe66fee7ecf91b35f6ae684f
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-05-16 19:18:54 +0300

    Created 'Array Elimination' by A2SV Hub

---
+++ 16-May-2025/Array Elimination 357091.py
@@ -0,0 +1,63 @@
+# Problem: Array Elimination - https://codeforces.com/contest/1601/problem/A
+
+#==================== BASIC IMPORTS ====================
+import sys, math, threading, heapq
+from operator import itemgetter
+from itertools import combinations, permutations
+from collections import Counter, defaultdict, deque
+from functools import cmp_to_key, reduce
+from bisect import bisect_left, bisect_right
+
+# ==================== FAST I/O ====================
+input = sys.stdin.readline
+output = sys.stdout.write
+
+# ==================== QUICK INPUT ====================
+INT = lambda: int(input().strip())
+LIST = lambda: list(map(int, input().split()))
+CHAR_LIST = lambda: list(input().strip())
+LINE = lambda: input().strip()
+MATRIX = lambda n: [LIST() for _ in range(n)]
+CHAR_MATRIX = lambda n: [list(input().strip()) for _ in range(n)]
+
+# ==================== CONSTANTS & UTILS ====================
+MOD = 10**9 + 7
+INF = float('inf')
+
+def yesNo(boolean): output("YES\n" if boolean else "NO\n")
+
+def inBound(x, y, n, m): return 0 <= x < n and 0 <= y < m
+
+# directions for grids
+dirs_4 = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+dirs_8 = [(-1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 0), (0, 1), (1, 1)]
+
+# ==================== SOLUTION ====================
+def solve():
+    n = INT()
+    nums = LIST()
+    setbits = [0] * 32
+    for num in nums:
+        idx = 0
+        curr = num
+        while curr:
+            setbits[idx] += curr & 1
+            curr >>= 1
+            idx += 1
+    
+    max_k = reduce(math.gcd, setbits)
+    ans = []
+    for i in range(1, n + 1):
+        if max_k % i == 0:
+            ans.append(i)
+    output(f"{' '.join(map(str, ans))}\n")
+
+# ==================== MAIN ====================
+def main():
+    T = 1
+    T = INT()
+    for _ in range(T):
+        solve()
+
+if __name__ == "__main__":
+    main()
