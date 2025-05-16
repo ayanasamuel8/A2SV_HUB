2aa3433eeaf46d836e0cbe099e6c8963cd935213
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-05-16 17:20:40 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 16-May-2025/Cirno's Perfect Bitmasks Classroom 356976.py
@@ -0,0 +1,60 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+#==================== BASIC IMPORTS ====================
+import sys, math, threading, heapq
+from operator import itemgetter
+from itertools import combinations, permutations
+from collections import Counter, defaultdict, deque
+from functools import cmp_to_key
+from bisect import bisect_left, bisect_right
+
+# ==================== FAST I/O ====================
+input = sys.stdin.readline
+output = sys.stdout.write
+
+# ==================== QUICK INPUT ====================
+INT = lambda: int(input().strip())
+LIST = lambda: list(map(int, input().split()))
+CHAR_LIST = lambda: list(input().strip())
+LINE = lambda: input().strip()
+MATRIX = lambda n: [LIST() for _ in range(n)]
+CHAR_MATRIX = lambda n: [list(input().strip()) for _ in range(n)]
+
+# ==================== CONSTANTS & UTILS ====================
+MOD = 10**9 + 7
+INF = float('inf')
+
+def yesNo(boolean): output("YES\n" if boolean else "NO\n")
+
+def inBound(x, y, n, m): return 0 <= x < n and 0 <= y < m
+
+# directions for grids
+dirs_4 = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+dirs_8 = [(-1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 0), (0, 1), (1, 1)]
+
+# ==================== SOLUTION ====================
+def solve():
+    n = INT()
+    # nums = LIST()
+    if bin(n).count('1') == 1:
+        start = 0
+        while True:
+            if n & (1 << start) == 0:
+                n |= (1 << start)
+                break
+            start += 1
+        return n
+
+    for i in range(33):
+        if n & (1 << i):
+            return 2 ** i
+
+# ==================== MAIN ====================
+def main():
+    T = 1
+    T = INT()
+    for _ in range(T):
+        output(f"{solve()}\n")
+
+if __name__ == "__main__":
+    main()
