ad4857be9868997406970b86fabf85bdf6d7a788
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-05-23 08:43:41 +0300

    Created 'Serval and The Formula' by A2SV Hub

---
+++ 23-May-2025/Serval and The Formula 359126.py
@@ -0,0 +1,57 @@
+# Problem: Serval and The Formula - https://codeforces.com/problemset/problem/2085/C
+
+#==================== BASIC IMPORTS ====================
+import sys, math, threading, heapq
+from operator import itemgetter
+from itertools import combinations, permutations
+from collections import Counter, defaultdict, deque
+from functools import cmp_to_key
+from bisect import bisect_left, bisect_right
+
+# ==================== FAST I/O ====================
+input = sys.stdin.readline
+output = sys.stdout.write
+
+# ==================== QUICK INPUT ====================
+INT = lambda: int(input().strip())
+LIST = lambda: list(map(int, input().split()))
+CHAR_LIST = lambda: list(input().strip())
+LINE = lambda: input().strip()
+MATRIX = lambda n: [LIST() for _ in range(n)]
+CHAR_MATRIX = lambda n: [list(input().strip()) for _ in range(n)]
+
+# ==================== CONSTANTS & UTILS ====================
+MOD = 10**9 + 7
+INF = float('inf')
+
+def yesNo(boolean): output("YES\n" if boolean else "NO\n")
+
+def inBound(x, y, n, m): return 0 <= x < n and 0 <= y < m
+
+# directions for grids
+dirs_4 = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+dirs_8 = [(-1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 0), (0, 1), (1, 1)]
+
+# ==================== SOLUTION ====================
+def solve():
+    # n = INT()
+    # nums = LIST()
+    n, m = LIST()
+    if n == m:
+        output(f"-1\n")
+        return
+    length = max(n, m).bit_length()
+    output(f"{(1 << length) - max(n, m)}\n")
+            
+        
+
+
+# ==================== MAIN ====================
+def main():
+    T = 1
+    T = INT()
+    for _ in range(T):
+        solve()
+
+if __name__ == "__main__":
+    main()
