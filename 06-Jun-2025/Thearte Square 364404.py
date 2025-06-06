d648c20d37b96cea916f80d8ba82e76f1f1958b5
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-06-06 23:53:39 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 06-Jun-2025/Thearte Square 364404.py
@@ -0,0 +1,52 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+#==================== BASIC IMPORTS ====================
+import sys, math, threading, heapq
+from operator import itemgetter
+from itertools import combinations, permutations
+from collections import Counter, defaultdict, deque
+from functools import cmp_to_key
+from bisect import bisect_left, bisect_right
+
+# ==================== FAST I/O ====================
+input = sys.stdin.readline
+output = sys.stdout.write
+
+# ==================== QUICK INPUT ====================
+INT = lambda: int(input().strip())
+LIST = lambda: list(map(int, input().split()))
+CHAR_LIST = lambda: list(input().strip())
+LINE = lambda: input().strip()
+MATRIX = lambda n: [LIST() for _ in range(n)]
+CHAR_MATRIX = lambda n: [list(input().strip()) for _ in range(n)]
+
+# ==================== CONSTANTS & UTILS ====================
+MOD = 10**9 + 7
+INF = float('inf')
+
+def yesNo(boolean): output("YES\n" if boolean else "NO\n")
+
+def inBound(x, y, n, m): return 0 <= x < n and 0 <= y < m
+
+# directions for grids
+dirs_4 = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+dirs_8 = [(-1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 0), (0, 1), (1, 1)]
+
+# ==================== SOLUTION ====================
+def solve():
+    # n = INT()
+    # nums = LIST()
+    n, m, a = LIST()
+    height = math.ceil(n / a)
+    width = math.ceil(m / a)
+    output(f"{height * width}")
+
+# ==================== MAIN ====================
+def main():
+    T = 1
+    # T = INT()
+    for _ in range(T):
+        solve()
+
+if __name__ == "__main__":
+    main()
