f3b18165dd98e904f8ccf574576156c82437eac5
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-08-01 20:24:04 +0300

    Created 'Check if array is sorted' by A2SV Hub

---
+++ 01-Aug-2025/Check if array is sorted 381119.py
@@ -0,0 +1,8 @@
+# Problem: Check if array is sorted - https://practice.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1
+
+class Solution:
+    def isSorted(self, arr) -> bool:
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                return False
+        return True
