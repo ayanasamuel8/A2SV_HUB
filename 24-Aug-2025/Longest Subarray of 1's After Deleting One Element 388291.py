c3bd57f4db2454ba1ea6834ad873cf49c316b4a0
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-08-24 16:54:46 +0300

    Created 'Longest Subarray of 1's After Deleting One Element' by A2SV Hub

---
+++ 24-Aug-2025/Longest Subarray of 1's After Deleting One Element 388291.py
@@ -0,0 +1,18 @@
+# Problem: Longest Subarray of 1's After Deleting One Element - https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        if 0 not in nums:
+            return n - 1
+        
+        left = 0
+        zero_count = 0
+        max_count = 0
+        for right in range(n):
+            zero_count += 1 - nums[right]
+            while zero_count > 1:
+                zero_count -= 1 - nums[left]
+                left += 1
+            max_count = max(max_count, right - left + 1 if zero_count == 0 else right - left)
+        return max_count
