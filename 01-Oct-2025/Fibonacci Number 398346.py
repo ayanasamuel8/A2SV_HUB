31a6105fc20cbcaf01bf619f01389ee2805302ab
Ayana Samuel <125457325+ayanasamuel8@users.noreply.github.com>
2025-10-01 10:36:51 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 01-Oct-2025/Fibonacci Number 398346.py
@@ -0,0 +1,10 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [0] * (n + 2)
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        
+        return dp[n]
